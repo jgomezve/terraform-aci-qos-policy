cb231b657b5ea1303dd441fb72880dff8d073977
danischm <danischm@cisco.com>
2022-10-13 16:45:26 +0200

    Fix cos value validation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1 (unreleased)
+
+- Fix CoS value validation
+
 ## 0.1.0
 
 - Initial release
--- variables.tf
+++ variables.tf
@@ -41,7 +41,7 @@ variable "description" {
 }
 
 variable "dscp_priority_maps" {
-  description = "QoS Policy DSCP Priority Maps. Allowed values `dscp_from`, `dscp_to` and `dscp_target` : `unspecified`, `CS0`, `CS1`, `AF11`, `AF12`, `AF13`, `CS2`, `AF21`, `AF22`, `AF23`, `CS4`, `AF41`, `AF42`, `AF43`, `CS5`, `VA`, `EF`, `CS6` `CS7` or a number between 0 and 63. Allowed values `priority`: `unspecified`, `level1`, `level2`, `level3`, `level4`, `level5` or `level6`. Allowed values `cos_target`: 0-7"
+  description = "QoS Policy DSCP Priority Maps. Allowed values `dscp_from`, `dscp_to` and `dscp_target` : `unspecified`, `CS0`, `CS1`, `AF11`, `AF12`, `AF13`, `CS2`, `AF21`, `AF22`, `AF23`, `CS4`, `AF41`, `AF42`, `AF43`, `CS5`, `VA`, `EF`, `CS6` `CS7` or a number between 0 and 63. Allowed values `priority`: `unspecified`, `level1`, `level2`, `level3`, `level4`, `level5` or `level6`. Allowed values `cos_target`: `unspecified` or a number between 0 and 7."
   type = list(object({
     dscp_from   = string
     dscp_to     = optional(string)
@@ -81,14 +81,14 @@ variable "dscp_priority_maps" {
 
   validation {
     condition = alltrue([
-      for pm in var.dscp_priority_maps : pm.cos_target == null || try(pm.cos_target >= 0 && pm.cos_target <= 7, false)
+      for pm in var.dscp_priority_maps : pm.cos_target == "unspecified" || try(pm.cos_target >= 0 && pm.cos_target <= 7, false)
     ])
-    error_message = "`cos_target`: Minimum value: `0`. Maximum value: `7`."
+    error_message = "`cos_target`: Allowed values are `unspecified` or a number between 0 and 7."
   }
 }
 
 variable "dot1p_classifiers" {
-  description = "QoS Policy DSCP Dot1p Classifiers. Allowed values `dot1p_from`, `dot1p_to` and `cos_target`: 0-7. Allowed values `dscp_target` : `unspecified`, `CS0`, `CS1`, `AF11`, `AF12`, `AF13`, `CS2`, `AF21`, `AF22`, `AF23`, `CS4`, `AF41`, `AF42`, `AF43`, `CS5`, `VA`, `EF`, `CS6`, `CS7` or a number between 0 and 63. Allowed values `priority`: `unspecified`, `level1`, `level2`, `level3`, `level4`, `level5` or `level6`."
+  description = "QoS Policy DSCP Dot1p Classifiers. Allowed values `dot1p_from`, `dot1p_to` and `cos_target`: `unspecified` or a number between 0 and 7. Allowed values `dscp_target` : `unspecified`, `CS0`, `CS1`, `AF11`, `AF12`, `AF13`, `CS2`, `AF21`, `AF22`, `AF23`, `CS4`, `AF41`, `AF42`, `AF43`, `CS5`, `VA`, `EF`, `CS6`, `CS7` or a number between 0 and 63. Allowed values `priority`: `unspecified`, `level1`, `level2`, `level3`, `level4`, `level5` or `level6`."
   type = list(object({
     dot1p_from  = string
     dot1p_to    = optional(string)
@@ -102,14 +102,14 @@ variable "dot1p_classifiers" {
     condition = alltrue([
       for dot1p in var.dot1p_classifiers : try(dot1p.dot1p_from >= 0 && dot1p.dot1p_from <= 7, false) || dot1p.dot1p_from == "unspecified"
     ])
-    error_message = "`dot1p_from`: Minimum value: `0`. Maximum value: `7`."
+    error_message = "`dot1p_from`: Allowed values are `unspecified` or a number between 0 and 7."
   }
 
   validation {
     condition = alltrue([
       for dot1p in var.dot1p_classifiers : dot1p.dot1p_to == null || try(dot1p.dot1p_to >= 0 && dot1p.dot1p_to <= 7, false) || dot1p.dot1p_to == "unspecified"
     ])
-    error_message = "`dot1p_to`: Minimum value: `0`. Maximum value: `7`."
+    error_message = "`dot1p_to`: Allowed values are `unspecified` or a number between 0 and 7."
   }
 
   validation {
@@ -128,8 +128,8 @@ variable "dot1p_classifiers" {
 
   validation {
     condition = alltrue([
-      for dot1p in var.dot1p_classifiers : dot1p.cos_target == null || try(dot1p.cos_target >= 0 && dot1p.cos_target <= 7, false)
+      for dot1p in var.dot1p_classifiers : dot1p.cos_target == "unspecified" || try(dot1p.cos_target >= 0 && dot1p.cos_target <= 7, false)
     ])
-    error_message = "`cos_target`: Minimum value: `0`. Maximum value: `7`."
+    error_message = "`cos_target`: Allowed values are `unspecified` or a number between 0 and 7."
   }
 }
