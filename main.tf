992c82e495f526c115471d66fbf5d846ab36c6a0
danischm <danischm@cisco.com>
2023-04-08 09:34:58 +0200

    Fix resource dependencies

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aci_rest_managed" "qosCustomPol" {
 
 resource "aci_rest_managed" "qosDscpClass" {
   for_each   = { for pm in var.dscp_priority_maps : "${pm.dscp_from}-${pm.dscp_to != null ? pm.dscp_to : pm.dscp_from}" => pm }
-  dn         = "${aci_rest_managed.qosCustomPol.id}/dcsp-${each.value.dscp_from}-${each.value.dscp_to != null ? each.value.dscp_to : each.value.dscp_from}"
+  dn         = "${aci_rest_managed.qosCustomPol.dn}/dcsp-${each.value.dscp_from}-${each.value.dscp_to != null ? each.value.dscp_to : each.value.dscp_from}"
   class_name = "qosDscpClass"
   content = {
     from      = each.value.dscp_from
@@ -23,7 +23,7 @@ resource "aci_rest_managed" "qosDscpClass" {
 
 resource "aci_rest_managed" "qosDot1PClass" {
   for_each   = { for dot1p in var.dot1p_classifiers : "${dot1p.dot1p_from}-${dot1p.dot1p_to != null ? dot1p.dot1p_to : dot1p.dot1p_from}" => dot1p }
-  dn         = "${aci_rest_managed.qosCustomPol.id}/dot1P-${each.value.dot1p_from}-${each.value.dot1p_to != null ? each.value.dot1p_to : each.value.dot1p_from}"
+  dn         = "${aci_rest_managed.qosCustomPol.dn}/dot1P-${each.value.dot1p_from}-${each.value.dot1p_to != null ? each.value.dot1p_to : each.value.dot1p_from}"
   class_name = "qosDot1PClass"
   content = {
     from      = each.value.dot1p_from
